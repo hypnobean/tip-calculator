2bebdb5f830ec089e61059cb8b59ba27c83314d2
hypnobean <115379768+hypnobean@users.noreply.github.com>
2023-02-16 11:09:14 -0500

    Update index.js
    Housekeeping (variable definitions and functions
    Got rid of error when reset is clicked (this.select() was causing error on reset button)
    Fixed error messages to keep them consistent in different scenarios
    Added error for when custom tip value is NaN

--- index.js
+++ index.js
@@ -1,32 +1,52 @@
-const theForm = document.forms["tipCalc"];
+// Define the input and calc variables
 let bill = 0;
 let tipButtonValue = 0;
 let customTipValue = 0;
 let totalBillTipPerPerson = 0;
 let totalTipAmount = 0;
 let noOfPeople = 1;
+// Define the inputs
+const theForm = document.forms["tipCalc"];
+const billInput = theForm.elements["bill"];
+const tipButton = theForm.elements["tip-btn"];
+const customTipInput = theForm.elements["customPerCent"];
+const noOfPeopleInput = theForm.elements["noOfPeople"];
+// Define the screen update containers
+const displayTotalTip = document.getElementById("tipTotal");
+const displayTotalPerPerson = document.getElementById("totalPerPerson");
+const billCannotBeZero = document.getElementsByClassName("zero")[0];
+const noOfPeopleCannotBeZero = document.getElementsByClassName("zero")[1];
 
 
 // Bill total input value
-theForm.elements["bill"].addEventListener("input", function() {
-    bill = Number(theForm.elements[0].value);
+billInput.addEventListener("input", function() {
+    bill = Number(billInput.value);
     console.log(bill);
+    billError();
+    noOfPeopleError();
 });
 
 
 
 // Get value of button clicked, clear custom tip input
-for (var i = 0; i < theForm.elements["tip-btn"].length; i++)    {
-    theForm.elements["tip-btn"][i].addEventListener("input", function() {
+for (var i = 0; i < tipButton.length; i++)    {
+    tipButton[i].addEventListener("input", function() {
         tipButtonValue = Number((this.value) / 100);
         console.log(tipButtonValue);
+        console.log(this.checked);
+        billError();
+        noOfPeopleError();
 
     })
-    theForm.elements["tip-btn"][i].addEventListener("click", function() {
+    tipButton[i].addEventListener("click", function() {
         theForm.elements["customPerCent"].value = "";
         customTipValue = 0;
-        })
-    }
+        billError();
+        noOfPeopleError();
+    })
+}
+
+// Toggle tip buttons
 
 
 // Get Value of Custom Tip
@@ -37,7 +57,7 @@ theForm.elements["customPerCent"].addEventListener("input", function() {
 
 
 // When custom tip is clicked, clear tip buttons
-theForm.elements["customPerCent"].addEventListener("click", function() {
+customTipInput.addEventListener("click", function() {
     for (var i = 0; i < document.querySelectorAll('input[type="radio"]').length; i++) {
         document.querySelectorAll('input[type="radio"]')[i].checked = false;
         tipButtonValue = 0;
@@ -47,8 +67,8 @@ theForm.elements["customPerCent"].addEventListener("click", function() {
 
 
 // Number of People
-theForm.elements["noOfPeople"].addEventListener("input", function() {
-    noOfPeople = Number(theForm.elements["noOfPeople"].value);
+noOfPeopleInput.addEventListener("input", function() {
+    noOfPeople = Number(noOfPeopleInput.value);
     console.log(noOfPeople);
 });
 
@@ -58,20 +78,25 @@ theForm.elements["noOfPeople"].addEventListener("input", function() {
 // Make sure that custom tip zero value still calculates 
 // MAKE SURE THAT CUSTOM TIP IS ZERO'D WHEN EVERYTHING IN IT IS DELETED
 function updateTipTotal() {
-    if (theForm.elements["customPerCent"] && theForm.elements["customPerCent"].value) { // check this
+    if (customTipInput && customTipInput.value) { // check this
         totalTipAmount = bill * customTipValue;
     }
     else if (tipButtonValue > 0) {
         totalTipAmount = bill * tipButtonValue;
     }
-
-    document.getElementById("tipTotal").innerHTML = "$" + parseFloat(totalTipAmount).toFixed(2);
+    displayTotalTip.innerHTML = "$" + parseFloat(totalTipAmount).toFixed(2);
+    billError();
+    noOfPeopleError();
+    customTipError();
 }
 
 
 function updateTotalPerPerson() {
     totalBillTipPerPerson = (bill + totalTipAmount) / noOfPeople;
-    document.getElementById("totalPerPerson").innerHTML = "$" + parseFloat(totalBillTipPerPerson).toFixed(2);
+    displayTotalPerPerson.innerHTML = "$" + parseFloat(totalBillTipPerPerson).toFixed(2);
+    billError();
+    noOfPeopleError();
+    customTipError();
 }
 
 
@@ -88,56 +113,70 @@ for (i = 0; i < theForm.elements.length; i++) {
 
 
 // Highlight text in text boxes for easy overwriting on mobile
-for (i = 0; i < theForm.elements.length; i++) {
-    theForm.elements[i].addEventListener("click", function() {
+for (i = 0; i < document.querySelectorAll('input[type="text"]').length; i++) {
+    document.querySelectorAll('input[type="text"]')[i].addEventListener("click" , function () {
         this.select();
-    }, false);
+    })
 }
 
 // Get reset button to clear all values and update innerHTML back to $0.00
-theForm.elements["reset"].addEventListener("click", function() {
+function clearAll() {
     bill = 0;
     tipButtonValue = 0;
     customTipValue = 0;
     totalBillTipPerPerson = 0;
     totalTipAmount = 0;
     noOfPeople = 1;
-    document.getElementById("tipTotal").innerHTML = "$0.00";
-    document.getElementById("totalPerPerson").innerHTML = "$0.00";
-})
+    displayTotalTip.innerHTML = "$0.00";
+    displayTotalPerPerson.innerHTML = "$0.00";
+}
 
-// Error for: Number of people can't be zero or letters
-theForm.elements["noOfPeople"].addEventListener("input", function() {
-    if (noOfPeople < 1) {
-        document.getElementsByClassName("zero")[1].innerHTML = "Can't be zero";
-        theForm.elements["noOfPeople"].style.border = "2px solid red";
-        document.getElementById("totalPerPerson").innerHTML = "Error";
-    }
-    else if (isNaN(noOfPeople)) {
-        document.getElementsByClassName("zero")[1].innerHTML = "Please input numbers only";
-        document.getElementById("totalPerPerson").innerHTML = "Error";
-    }
-    else {
-        document.getElementsByClassName("zero")[1].innerHTML = "";
-        theForm.elements["noOfPeople"].style.border = "";
-    }
-})
+
+theForm.elements["reset"].addEventListener("click", clearAll);
 
 // Get error message when bill input is anything but numbers
-theForm.elements["bill"].addEventListener("input", function() {
+function billError() {
     if (bill < 1) {
-        document.getElementsByClassName("zero")[0].innerHTML = "Can't be zero";
-        theForm.elements["bill"].style.border = "2px solid red";
-        document.getElementById("totalPerPerson").innerHTML = "Error";
-        document.getElementById("tipTotal").innerHTML = "Error";
+        billCannotBeZero.innerHTML = "Can't be zero";
+        billInput.style.border = "2px solid red";
+        displayTotalPerPerson.innerHTML = "$0.00";
+        displayTotalTip.innerHTML = "$0.00";
     }
     else if (isNaN(bill)) {
-        document.getElementsByClassName("zero")[0].innerHTML = "Please input numbers only";
-        document.getElementById("totalPerPerson").innerHTML = "Error";
-        document.getElementById("tipTotal").innerHTML = "Error";
+        billCannotBeZero.innerHTML = "Please input numbers only";
+        displayTotalPerPerson.innerHTML = "Error";
+        displayTotalTip.innerHTML = "Error";
     }
     else {
-        document.getElementsByClassName("zero")[0].innerHTML = "";
-        theForm.elements["bill"].style.border = "";
+        billCannotBeZero.innerHTML = "";
+        billInput.style.border = "";
     }
-})
+}
+
+// Error for: Number of people can't be zero or letters
+function noOfPeopleError() {
+    if (noOfPeople < 1) {
+        noOfPeopleCannotBeZero.innerHTML = "Can't be zero";
+        noOfPeopleInput.style.border = "2px solid red";
+        displayTotalPerPerson.innerHTML = "Error";
+    }
+    else if (isNaN(noOfPeople)) {
+        noOfPeopleCannotBeZero.innerHTML = "Please input numbers only";
+        displayTotalPerPerson.innerHTML = "Error";
+    }
+    else {
+        noOfPeopleCannotBeZero.innerHTML = "";
+        noOfPeopleInput.style.border = "";
+    }
+}
+
+// Error if letter is entered in custom tip input
+function customTipError() {
+    if (isNaN(customTipValue)) {
+        customTipInput.style.border = "2px solid red";
+        displayTotalPerPerson.innerHTML = "Error";
+        displayTotalTip.innerHTML = "Error";
+    } else {
+        customTipInput.style.border = "";
+    }
+}
